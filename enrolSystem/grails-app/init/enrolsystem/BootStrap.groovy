fc9b8e7759d1fb12e5f2524a51738fbc61175932
Ubuntu <labstudent@ML-RefVm-847893.oyelevrdlche1fvt04t1wzs2mf.ax.internal.cloudapp.net>
2022-01-25 17:58:44 +0000

    Added bootstrapping. I put 2 courses that should be preloaded, however they arent being preloaded and im not getting any errors

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,37 @@
-package enrolsystem
+package com.enrol
 
 class BootStrap {
 
     def init = { servletContext ->
+
+	def computing = new Course(
+	
+	department: 'Computing',
+	courseTitle: 'Computing',
+	courseLeader: 'Dr Michelle Murphy',
+	courseCode: 'CS123',
+	studyMode: 'Fulltime',
+	startDate: new Date('09/09/2019'),
+	endDate: new Date('07/07/2023'),
+	description: '"Course involving different aspects of the computing industry, web development, system architectures, and project management."',
+	numberOfStudents: 15,
+	tuitionFees: 9000
+	).save()
+
+	def webDevelopment = new Course(
+
+	department: 'Computing',
+	courseTitle: 'Web Development',
+	courseLeader: 'Jacky',
+	courseCode: 'WD123',
+	studyMode: 'Fulltime',
+	startDate: new Date('09/09/2019'),
+	endDate: new Date('07/07/2023'),
+	description: '"Course involving different aspects of the computing industry, web development, system architectures, and project management."',
+	numberOfStudents: 15,
+	tuitionFees: 9000
+	).save()
+
     }
     def destroy = {
     }
